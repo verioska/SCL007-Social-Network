8273e9c5100b8fb7adc18b9ab2617fd2f9cddbbb
BarbaraRH <barbararamos@MacBook-Air-de-Barbara.local>
2019-02-07 23:37:40 -0300

    vuelta a 1 js

--- src/main.js
+++ src/main.js
@@ -1,13 +1,23 @@
-import {checkAuthState, registerUser, loginUser} from '../auth.js';
-import {saveRecipe, readRecipes} from '../data.js';
-
-
 document.getElementById("page1").style.display="none"
+
+// document.getElementById("loginButton").addEventListener("click", function(){
+//   document.getElementById("muro").style.display="none"
+//   document.getElementById("recipeContainer").style.display="block"
+// })
+
+// document.getElementById("enter").addEventListener("click", function(){
+//   document.getElementById("muro").style.display="block" 
+// })
+
+// document.getElementById("sendRecipe").addEventListener("click", function(){
+//   document.getElementById("muro").style.display="none"
+//   document.getElementById("recipeContainer").style.display="block"
+// })
 
 document.getElementById("cerrar").addEventListener("click", function(){
  
    document.getElementById("recipeContainer").style.display="none"
-    firebase.auth().signOut()
+  firebase.auth().signOut()
   .then(function(){
       console.log('Saliendo...')
   
@@ -18,40 +28,89 @@ document.getElementById("cerrar").addEventListener("click", function(){
   })
   })
 
-window.onload = () => {
-    checkAuthState((user)=>{
+
+
+
+
+/* document.getElementById("page1").style.display="none" */
+//auth.js
+
+const checkAuthState = (callback) => {
+  firebase.auth().onAuthStateChanged((user)=>{
     if(user){
-        loginOrRegister.style.display = "none";
-        app.style.display = "block";
-        readRecipesFromDatabase();
+      console.log("Hay un usuario > "+JSON.stringify(user));
+      callback(user);
     }else{
-        loginOrRegister.style.display = "block";
-        app.style.display = "none";
+      console.log("No está logueado");
+      callback(null);
     }
+  })
+};
+
+const registerUser = (email, password) => {
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then((user)=>{
+      console.log("Usuario registrado > "+JSON.stringify(user));
+    })
+    .catch((error) => {
+      console.error("Error > "+error.message);
     });
+}
+
+const loginUser = (email, password) => {
+  firebase.auth().signInWithEmailAndPassword(email, password)
+    .then((user)=>{
+      console.log("Usuario logueado > "+JSON.stringify(user));
+    })
+    .catch((error) => {
+      console.error("Error > "+error.message);
+    });
+    
+}
+
+
+
+//data.js
+
+
+
+let newRecipeKey = "";
+const saveRecipe = (recipeTitle, recipeImage, ownerName, insRecipe, recipeIngredients, recipeServes, prepTime, recipeCost) => {
+  newRecipeKey = firebase.database().ref('recipe/boasfdisfbsfahb').child('likes').push().key;
+
+  firebase.database().ref(`recipe/${newRecipeKey}`).set({
+    title : recipeTitle,
+    image : recipeImage,
+    owner : ownerName,
+    recipes: insRecipe,
+    Key: newRecipeKey,
+    Ingredients: recipeIngredients,
+    serves: recipeServes,
+    time: prepTime,
+    cost: recipeCost,
+      
+
+  });
 };
 
-const registerWithEmailAndPassword = () => {
-    const emailFromUser = emailTextfield.value;
-    const passwordFromUser = passwordTextfield.value;
-    registerUser(emailFromUser, passwordFromUser);
-};
-
-const loginUserWithEmailAndPassword = () => {
-    const emailFromUser = emailTextfield.value;
-    const passwordFromUser = passwordTextfield.value;
-    loginUser(emailFromUser, passwordFromUser);
-};
-
-
-/* const saveUserIntoDatabase = () => {
-  const userId = firebase.auth().currentUser.uid;
-  const userEmail = firebase.auth().currentUser.email;
-  saveUser(userId, userEmail);
+/* const deletePost = (id) => {
+  let recipeRef = firebase.database().ref('recipe/' + id);
+  recipeRef.remove()
 } */
+
+
+
+
+const readRecipes = (onRecipeChange) => {
+  var recipeRef = firebase.database().ref('recipe');
+  recipeRef.on('child_added', (recipe)=> {
+    onRecipeChange(recipe);
+  });
+};
 
 var uploader = document.getElementById('uploader');
 var fileButton = document.getElementById('fileButton');
+let imgUrl = "";
 
 fileButton.addEventListener('change', function(e) {
   var file = e.target.files[0];
@@ -102,16 +161,51 @@ fileButton.addEventListener('change', function(e) {
 
 })
 
+
+
+//main.js
+window.onload = () => {
+checkAuthState((user)=>{
+  if(user){
+    loginOrRegister.style.display = "none";
+    app.style.display = "block";
+    readRecipesFromDatabase();
+  }else{
+    loginOrRegister.style.display = "block";
+    app.style.display = "none";
+  }
+});
+};
+
+const registerWithEmailAndPassword = () => {
+const emailFromUser = emailTextfield.value;
+const passwordFromUser = passwordTextfield.value;
+registerUser(emailFromUser, passwordFromUser);
+};
+
+const loginUserWithEmailAndPassword = () => {
+const emailFromUser = emailTextfield.value;
+const passwordFromUser = passwordTextfield.value;
+loginUser(emailFromUser, passwordFromUser);
+};
+
+//nueva 
+const saveUserIntoDatabase = () => {
+  const userId = firebase.auth().currentUser.uid;
+  const userEmail = firebase.auth().currentUser.email;
+  saveUser(userId, userEmail);
+}
+
 const saveRecipesIntoDatabase = () => {
-    const recipeTitle = titleRecipe.value;
-    const recipeImage = imgUrl;
-    const ownerName = firebase.auth().currentUser.email;
-    const insRecipe = insRecipes.value;
-    const recipeIngredients = idIngredients.value;
-    const recipeServes = idServes.value;
-    const prepTime = idTime.value;
-    const recipeCost = idCost.value;
-    saveRecipe(recipeTitle, recipeImage, ownerName, insRecipe, recipeIngredients, recipeServes, prepTime, recipeCost);
+const recipeTitle = titleRecipe.value;
+const recipeImage = imgUrl;
+const ownerName = firebase.auth().currentUser.email;
+const insRecipe = insRecipes.value;
+const recipeIngredients = idIngredients.value;
+const recipeServes = idServes.value;
+const prepTime = idTime.value;
+const recipeCost = idCost.value;
+saveRecipe(recipeTitle, recipeImage, ownerName, insRecipe, recipeIngredients, recipeServes, prepTime, recipeCost);
 }
 
 const readRecipesFromDatabase = () => {
@@ -143,7 +237,32 @@ const readRecipesFromDatabase = () => {
   });
  }
 
+// const accessRecipesFromDatabase = () => {
+//   accessRecipes((id)=>{
+//       document.getElementById(id).addEventListener('click', function(){
+//           alert("holaMundo");
+//           document.getElementById("page1").style.display="block"
+//           document.getElementById("loginOrRegister").style.display="none"
+//           document.getElementById("app").style.display="none"
+//           let recipeRef = firebase.database().ref('recipe/' + id);
+//           page1.innerHTML = 
+//           `<h2>SABORES</h2>
+//           <h3>${recipeRef.val().title}</h3>
+//           <img id="link" src="${recipe.val().image}" style="width:300px"/>
+//           <p> ${recipeRef.val().recipes}</p> 
+//           `+page1.innerHTML;  
+                 
+//         })
+//   }
+
+//   )
+// }
+
+
+
 
 registerButton.addEventListener('click', registerWithEmailAndPassword);
 loginButton.addEventListener('click', loginUserWithEmailAndPassword);
+//nueva
+/* saveUserData.addEventListener('click', saveUserIntoDatabase)  */
 sendRecipe.addEventListener('click', saveRecipesIntoDatabase);
